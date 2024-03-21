6be4d240968c0f03ef7f3eb52fa28b6f54c745bc
SamyukthaMohan <yukthamohan03@gmail.com>
2024-03-21 21:38:08 +0530

    Day 7-Updating on Service class and controller class

---
+++ backend/my-backend/src/main/java/com/example/mybackend/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.mybackend.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mybackend.dto.UpdateRequest;
+import com.example.mybackend.model.User;
+import com.example.mybackend.repo.UserRepo;
+
+import io.micrometer.common.lang.NonNull;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    
+
+    public User createUser(@NonNull User user)   //()-->Parameter,passing data called argument
+    {
+        return userRepository.save(user);
+
+    }
+    
+    //getuser by email
+    public Optional <User> getUserByEmail(String email)
+    {
+        return userRepository.findByEmail(email);
+    }
+
+    //get list of users
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    //update User
+    public User updateUser(@NonNull String email,UpdateRequest updateRequest){
+        return userRepository.findByEmail(email)  //user data are fetched by email by findByEmil method.
+            .map(existingUser -> {
+                existingUser.setUsername(updateRequest.getUsername());  //this map method name will be updates in the existing user
+                existingUser.setEmail(updateRequest.getEmail());
+                existingUser.setPassword(updateRequest.getPassword());
+                return userRepository.save(existingUser);
+            })
+            .orElseThrow(() -> new RuntimeException("User not found with this email: " + email));
+    }
+
+    //deleteUser
+    public void removeUser(@NonNull Integer userId){
+        userRepository.deleteById(userId);
+    }
+}
